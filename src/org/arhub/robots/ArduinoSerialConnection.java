3cd5e4bb68ff02213bb6263537b78c14dc2722d0
Keaton Burleson <keaton.burleson@me.com>
2015-05-01 17:35:48 -0500

    Omni-drive support

--- src/org/arhub/robots/ArduinoSerialConnection.java
+++ src/org/arhub/robots/ArduinoSerialConnection.java
@@ -1,5 +1,7 @@
 package org.arhub.robots;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,7 +17,8 @@ public class ArduinoSerialConnection {
 
 	private SerialPort serialPort;
 	private List<String> ports;
-
+	private List<Integer> leftMotors;
+	private List<Integer> rightMotors;
 	public List<String> GetComPorts() {
 		
 		setPorts((List<String>) Arrays.asList(SerialPortList.getPortNames()));
@@ -27,8 +30,8 @@ public class ArduinoSerialConnection {
 	}
 
 	public void Connect(String comPort) throws SerialPortException {
-		serialPort = new SerialPort(comPort);
-	//	serialPort = new SerialPort("/dev/tty.HC-06-DevB");
+	//	serialPort = new SerialPort(comPort);
+		serialPort = new SerialPort("/dev/cu.wchusbserial620");
 		
 		try{
 			serialPort.openPort();
@@ -50,60 +53,77 @@ public class ArduinoSerialConnection {
 		for (ControlState controlState : controlStates) {
 			states.add(Integer.toString(controlState.value));
 		}
-		List<String> bytesArray = new ArrayList<String>();
+		List<String> bytesArray = new ArrayList<String>(5);
 		
+		int intBytesB = Integer.parseInt(states.get(0));
+		int intBytesX = Integer.parseInt(states.get(1));
+		int intBytesY = Integer.parseInt(states.get(2));
+		int intBytesZ = Integer.parseInt(states.get(3));
+		
+		int backLeftMotor = 0;
+		int backRightMotor = 0;
+		int frontLeftMotor = 0;
+		int frontRightMotor = 0;
+		
+		int outputButton = 0;
+		System.out.println("Fire Button: " + intBytesB);
+		System.out.println("X Axis: " + intBytesX);
+		System.out.println("Y Axis: " + intBytesY);
+		System.out.println("Z Axis: " + intBytesZ);
+	
+		if(betweenExclusive(intBytesX, 87, 92) && betweenExclusive(intBytesY, 87, 92)){
+		
+			//Zero'd Function;
+			
+			frontRightMotor = 90;
+			frontLeftMotor = 90;
+			backRightMotor = 90;
+			backLeftMotor = 90;
+			System.out.println("Zero");
+			
+		}
+		else{
+			double twoPI = 2 * Math.PI;
+			intBytesX = intBytesX - 90;
+			intBytesY = intBytesY - 90;
+			double theta = Math.atan((double)intBytesY/intBytesX);
+			double r = Math.sqrt((intBytesX * intBytesX) + (intBytesY * intBytesY));
+			frontRightMotor = (int) (r * Math.cos(theta)) + 90 + intBytesZ;
+			backRightMotor = (int) (r * Math.sin(twoPI - theta)) + 90 + intBytesZ;
+			backLeftMotor = (int) (r * Math.cos(twoPI - theta)) + 90 + intBytesZ;
+			frontLeftMotor = (int) (r * Math.sin(theta)) + 90 + intBytesZ;
+		}
+	
+		
+		if(intBytesB == 1){
+			outputButton = 0;
+			//Shoot catapult
+		}else{
+			outputButton = 90;
+		}
 		
 		if (serialPort != null) {
 			
-			int intBytes = Integer.parseInt(StringUtils.join(states, ",").replace(",", ""));
+			bytesArray.add(String.valueOf(backLeftMotor));
+			bytesArray.add(String.valueOf(backRightMotor));
+			bytesArray.add(String.valueOf(frontLeftMotor));
+			bytesArray.add(String.valueOf(frontRightMotor));
+			bytesArray.add(String.valueOf(outputButton));
 			
 		
-			System.out.println(intBytes);
-			if(betweenExclusive(intBytes, 8800, 9300) == true){
-				System.out.println("Zero");
-				
-				bytesArray.add(Integer.toString(0));
-				bytesArray.add(Integer.toString(0));
-				serialPort.writeBytes(((StringUtils.join(bytesArray, ",") + "\n").getBytes()));
-				bytesArray.clear();
-				
-			}else if(betweenExclusive(intBytes, 900, 1600)){
-				System.out.println("Left");
-		
-				
-					bytesArray.add(Integer.toString(255));
-					bytesArray.add(Integer.toString(0));
-					serialPort.writeBytes(((StringUtils.join(bytesArray, ",") + "\n").getBytes()));
-					bytesArray.clear();
-					
 			
-			}else if(betweenExclusive(intBytes, 60, 800)){
-				System.out.println("Backward");
-				bytesArray.add(Integer.toString(500));
-				bytesArray.add(Integer.toString(500));
-				serialPort.writeBytes(((StringUtils.join(bytesArray, ",") + "\n").getBytes()));
-				bytesArray.clear();
-			}else if(betweenExclusive(intBytes, 16116, 18100)){
-				System.out.println("Forward");
-				bytesArray.add(Integer.toString(255));
-				bytesArray.add(Integer.toString(255));
-				serialPort.writeBytes(((StringUtils.join(bytesArray, ",") + "\n").getBytes()));
-				bytesArray.clear();
-			}else if(betweenExclusive(intBytes, 38000, 133100)){
-				System.out.println("Right");
-				bytesArray.add(Integer.toString(0));
-				bytesArray.add(Integer.toString(255));
-				serialPort.writeBytes(((StringUtils.join(bytesArray, ",") + "\n").getBytes()));
-				bytesArray.clear();
-			}
-		//	serialPort.writeBytes(((StringUtils.join(bytesArray, ",") + "\n").getBytes()));
-			}
 	
-			//serialPort.writeBytes(((StringUtils.join(bytesArray, ",") + "\n").getBytes()));
+			
+			serialPort.writeBytes(((StringUtils.join(bytesArray, ",") + "\n").getBytes()));
+			bytesArray.clear();
+			
+			
+			}
 		
-//		System.out.println(StringUtils.join(states, ","));
-
-	}
+	
+}
+		
+	
 
 	public void disconnect() throws SerialPortException {
 		serialPort.closePort();
